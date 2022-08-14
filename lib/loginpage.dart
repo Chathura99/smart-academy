788683f8da6690bad1fb57b032fe8144382b4079
Chathura <chathura.madura11@gmail.com>
2022-08-14 21:44:52 +0530

    create login screen

---
+++ lib/loginpage.dart
@@ -0,0 +1,132 @@
+import 'dart:js_util';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late String _email;
+  late String _password;
+
+  // TextEditingController nameController = TextEditingController();
+  // TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Welcome to Unity Academy!',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                      // controller: nameController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          _email = value;
+                        });
+                      }),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                      obscureText: true,
+                      // controller: passwordController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          _password = value;
+                        });
+                      }),
+                ),
+                TextButton(
+                  onPressed: () {
+                    //forgot password screen
+                  },
+                  child: const Text(
+                    'Forgot Password',
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      child: const Text('Login'),
+                      onPressed: () {
+                        // print(nameController.text);
+                        // print(passwordController.text);
+                        // print("pw:"+_password+ "| email :" +_email);
+                        FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                                email: _email, password: _password)
+                            .then((user) => Navigator.of(context)
+                                .pushReplacementNamed('/home'))
+                            .catchError((e) {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return alert;
+                            },
+                          );
+
+                          // print(e);
+                        });
+                      },
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('Does not have account?'),
+                    TextButton(
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                    )
+                  ],
+                ),
+              ],
+            )));
+  }
+}
+
+// Create AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Login failed!"),
+    content: Text("Invalid credentials"),
+  );
+
