9f9be90311c3c02094a21773561680a81cf6993b
Chathura <chathura.madura11@gmail.com>
2022-10-09 01:36:38 +0530

    change profile page style

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -3,125 +3,61 @@ import 'package:flutter/material.dart';
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(4, 9, 35, 1),
-                Color.fromRGBO(39, 105, 171, 1),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Profile"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+        child: Column(
+          children: [
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
               ],
-              begin: FractionalOffset.bottomCenter,
-              end: FractionalOffset.topCenter,
             ),
-          ),
-        ),
-        Scaffold(
-          backgroundColor: Colors.transparent,
-          body: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 73),
+            Center(
+              child: Container(
+                child: Image.asset(
+                  '/profile.png',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
               child: Column(
                 children: [
-                  Row(
-                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.arrow_back),
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
+                  Text(
+                    'Chathura Manohara',
+                    style: TextStyle(
+                      color: Color.fromRGBO(39, 105, 171, 1),
+                      fontSize: 27,
+                    ),
                   ),
                   Text(
-                    'My Profile',
-                    textAlign: TextAlign.center,
+                    'Hello Student!',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 34,
-                      fontFamily: 'Nisebuschgardens',
+                      color: Color.fromRGBO(39, 105, 171, 1),
+                      fontSize: 15,
                     ),
                   ),
-                  SizedBox(
-                    height: 10,
+                  Divider(
+                    thickness: 2.5,
                   ),
-                  Container(
-                    height: height * 0.43,
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        double innerHeight = constraints.maxHeight;
-                        double innerWidth = constraints.maxWidth;
-                        return Stack(
-                          fit: StackFit.expand,
-                          children: [
-                            Positioned(
-                              top: 0,
-                              left: 0,
-                              right: 0,
-                              child: Center(
-                                child: Container(
-                                  child: Image.asset(
-                                    '/profile.png',
-                                    width: innerWidth * 0.45,
-                                    fit: BoxFit.fitWidth,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: height * 0.5,
-                    width: width,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.white,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            'Chathura Manohara',
-                            style: TextStyle(
-                              color: Color.fromRGBO(39, 105, 171, 1),
-                              fontSize: 27,
-                            ),
-                          ),
-                          Divider(
-                            thickness: 2.5,
-                          ),
-                          Text("School : "),
-                          Text("Grade : ")
-                        ],
-                      ),
-                    ),
-                  )
+                  Text("School : Maliyadeva College"),
+                  Text("Grade : 6")
                 ],
               ),
             ),
-          ),
-        )
-      ],
+          ],
+        ),
+      ),
     );
   }
 }
