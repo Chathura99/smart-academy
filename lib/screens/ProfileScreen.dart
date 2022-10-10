e785e8f7c486ca6a908679c201448a853f0872fe
Chathura <chathura.madura11@gmail.com>
2022-10-10 19:55:59 +0530

    create profile screen

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import "package:cloud_firestore/cloud_firestore.dart";
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  State<ProfileScreen> createState() => ProfileScreenState();
+}
+
+class ProfileScreenState extends State<ProfileScreen> {
+  var type = "student";
+  //To Do:  show data with selected user role part
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,17 +19,6 @@ class ProfileScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
         child: Column(
           children: [
-            Row(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
             Center(
               child: Container(
                 child: Image.asset(
@@ -40,18 +37,34 @@ class ProfileScreen extends StatelessWidget {
                       fontSize: 27,
                     ),
                   ),
-                  Text(
-                    'Hello Student!',
-                    style: TextStyle(
-                      color: Color.fromRGBO(39, 105, 171, 1),
-                      fontSize: 15,
-                    ),
-                  ),
+                  type == "student"
+                      ? Text(
+                          'Hello Student!',
+                          style: TextStyle(
+                            color: Color.fromRGBO(39, 105, 171, 1),
+                            fontSize: 15,
+                          ),
+                        )
+                      : Text(
+                          'Hello Teacher!',
+                          style: TextStyle(
+                            color: Color.fromRGBO(39, 105, 171, 1),
+                            fontSize: 15,
+                          ),
+                        ),
                   Divider(
                     thickness: 2.5,
                   ),
                   Text("School : Maliyadeva College"),
-                  Text("Grade : 6")
+                  Text("Grade : 6"),
+                  Divider(
+                    thickness: 2.5,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("edited!");
+                      },
+                      child: Text("Edit"))
                 ],
               ),
             ),
@@ -59,5 +72,6 @@ class ProfileScreen extends StatelessWidget {
         ),
       ),
     );
+    ;
   }
 }
