89b0b1d5e1a537a15ed3a93a4c027db42e70be32
Chathura <chathura.madura11@gmail.com>
2022-10-13 01:48:17 +0530

    save articles to db

--- lib/screens/articalscreen/add_artical.dart
+++ lib/screens/articalscreen/add_artical.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import "package:cloud_firestore/cloud_firestore.dart";
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AddArtical extends StatefulWidget {
   const AddArtical({Key? key}) : super(key: key);
@@ -10,6 +12,7 @@ class AddArtical extends StatefulWidget {
 class _AddArticalState extends State<AddArtical> {
   late String _title;
   late String _content;
+  late String _username;
 
   TextEditingController titleController = TextEditingController();
   TextEditingController contentController = TextEditingController();
@@ -18,8 +21,8 @@ class _AddArticalState extends State<AddArtical> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Articals"),
-        ),
+            title: Text("Articals"),
+            backgroundColor: Color.fromRGBO(39, 105, 171, 1)),
         body: Padding(
             padding: const EdgeInsets.all(10),
             child: ListView(
@@ -65,8 +68,8 @@ class _AddArticalState extends State<AddArtical> {
                     child: ElevatedButton(
                       child: const Text('Publish'),
                       onPressed: () {
-                        print(titleController.text);
-                        print(contentController.text);
+                        addArticle(titleController.text, contentController.text,
+                            context);
                       },
                     )),
               ],
@@ -74,8 +77,69 @@ class _AddArticalState extends State<AddArtical> {
   }
 }
 
+void addArticle(String title, String text, BuildContext context) async {
+  // get email
+  var usermail = await retrieveUserEmail();
+  print(usermail);
+  // get username
+
+  var name = "";
+  await FirebaseFirestore.instance
+      .collection('users')
+      .where('email', isEqualTo: usermail)
+      .get()
+      .then((QuerySnapshot querySnapshot) {
+    querySnapshot.docs.forEach((doc) {
+      name = doc["name"];
+    });
+  });
+
+  print(name);
+// save to database
+  print(DateTime.now());
+  FirebaseFirestore.instance.collection('articles').add({
+    'text': text,
+    'title': title,
+    'like': 0,
+    'unlike': 0,
+    'username': name,
+    'email': usermail,
+    'hour': DateTime.now()
+  }).then((value) {
+    // Navigator.pop(context);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert(context);
+      },
+    );
+  }).catchError((e) {
+    print(e);
+  });
+}
+
+// get email from shared pref
+retrieveUserEmail() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String value = prefs.getString("useremail") ?? "";
+  return value;
+}
+
 // Create AlertDialog
-AlertDialog alert = AlertDialog(
-  title: Text("Success!"),
-  content: Text("Artical successfully added"),
-);
+AlertDialog alert(context) => AlertDialog(
+      title: Text("Success!"),
+      content: Text("Artical successfully added"),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
+          },
+          child: Container(
+            color: Color.fromARGB(255, 255, 255, 255),
+            padding: const EdgeInsets.all(5),
+            child: const Text("OK"),
+          ),
+        ),
+      ],
+    );
