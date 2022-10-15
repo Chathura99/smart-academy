8dc93bc5f89315d9798986463978ad2326f32b3c
Chathura <chathura.madura11@gmail.com>
2022-10-16 00:16:13 +0530

    get data to profile from db

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 import "package:cloud_firestore/cloud_firestore.dart";
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
   State<ProfileScreen> createState() => ProfileScreenState();
 }
 
+var _name;
+var _type;
+var _email;
+
 class ProfileScreenState extends State<ProfileScreen> {
-  var type = "student";
+  @override
+  void initState() {
+    retrieveUserEmail();
+    // getProfileData();
+    super.initState();
+  }
+
+  retrieveUserEmail() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String value = prefs.getString("useremail") ?? "";
+    // set user's email
+    setState(() {
+      _email = value;
+    });
+    // set user's name
+    await FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: _email)
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        setState(() {
+          _name = doc["name"];
+          _type = doc['role'];
+        });
+      });
+    });
+  }
+
   //To Do:  show data with selected user role part
   @override
   Widget build(BuildContext context) {
@@ -31,13 +64,13 @@ class ProfileScreenState extends State<ProfileScreen> {
               child: Column(
                 children: [
                   Text(
-                    'Chathura Manohara',
+                    _name,
                     style: TextStyle(
                       color: Color.fromRGBO(39, 105, 171, 1),
                       fontSize: 27,
                     ),
                   ),
-                  type == "student"
+                  _type == "student"
                       ? Text(
                           'Hello Student!',
                           style: TextStyle(
@@ -55,8 +88,9 @@ class ProfileScreenState extends State<ProfileScreen> {
                   Divider(
                     thickness: 2.5,
                   ),
-                  Text("School : Maliyadeva College"),
-                  Text("Grade : 6"),
+                  // Text("School : Maliyadeva College"),
+                  // Text("Grade : 6"),
+                  Text("Email : ${_email}"),
                   Divider(
                     thickness: 2.5,
                   ),
@@ -75,3 +109,34 @@ class ProfileScreenState extends State<ProfileScreen> {
     ;
   }
 }
+
+// void getProfileData() async {
+//   // get email
+//   var usermail = await retrieveUserEmail();
+//   print(usermail);
+//   // get username
+
+//   var name = "";
+//   await FirebaseFirestore.instance
+//       .collection('users')
+//       .where('email', isEqualTo: usermail)
+//       .get()
+//       .then((QuerySnapshot querySnapshot) {
+//     querySnapshot.docs.forEach((doc) {
+//       name = doc["name"];
+//     });
+//   });
+
+//   await FirebaseFirestore.instance
+//       .collection('articles')
+//       .get()
+//       .then((QuerySnapshot querySnapshot) {
+//     querySnapshot.docs.forEach((doc) {
+//       // setState(() {
+//       //   _name = doc['name'];
+//       //   _role = doc['role'];
+//       //   _email = doc['email'];
+//       // });
+//     });
+//   });
+// }
