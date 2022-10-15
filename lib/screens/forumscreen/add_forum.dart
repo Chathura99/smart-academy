a3e9f97ca666d622451318180122c6427a95ce73
Chathura <chathura.madura11@gmail.com>
2022-10-15 20:10:01 +0530

    add froum by teacher

--- lib/screens/forumscreen/add_forum.dart
+++ lib/screens/forumscreen/add_forum.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import "package:cloud_firestore/cloud_firestore.dart";
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AddForum extends StatefulWidget {
   const AddForum({Key? key}) : super(key: key);
@@ -17,7 +19,6 @@ class _AddForumState extends State<AddForum> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
         body: Padding(
             padding: const EdgeInsets.all(10),
             child: ListView(
@@ -46,7 +47,6 @@ class _AddForumState extends State<AddForum> {
                 Container(
                   padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
                   child: TextField(
-                      obscureText: true,
                       controller: descriptionController,
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
@@ -66,6 +66,7 @@ class _AddForumState extends State<AddForum> {
                       onPressed: () {
                         print(titleController.text);
                         print(descriptionController.text);
+                        addForum(titleController.text,descriptionController.text,context);
                       },
                     )),
               ],
@@ -73,8 +74,67 @@ class _AddForumState extends State<AddForum> {
   }
 }
 
+void addForum(String title, String des, BuildContext context) async {
+  // get email
+  var usermail = await retrieveUserEmail();
+  print(usermail);
+  // get username
+
+  var name = "";
+  await FirebaseFirestore.instance
+      .collection('users')
+      .where('email', isEqualTo: usermail)
+      .get()
+      .then((QuerySnapshot querySnapshot) {
+    querySnapshot.docs.forEach((doc) {
+      name = doc["name"];
+    });
+  });
+
+  print(name);
+// save to database
+  print(DateTime.now());
+  FirebaseFirestore.instance.collection('forums').add({
+    'description': des,
+    'title': title,
+    'username': name,
+    'email': usermail,
+    'hour': DateTime.now(),
+    'reply': {}
+  }).then((value) {
+    // Navigator.pop(context);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert(context);
+      },
+    );
+  }).catchError((e) {
+    print(e);
+  });
+}
+
+// get email from shared pref
+retrieveUserEmail() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String value = prefs.getString("useremail") ?? "";
+  return value;
+}
+
 // Create AlertDialog
-AlertDialog alert = AlertDialog(
-  title: Text("Success!"),
-  content: Text("Artical successfully added"),
-);
+AlertDialog alert(context) => AlertDialog(
+      title: Text("Success!"),
+      content: Text("Artical successfully added"),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Container(
+            color: Color.fromARGB(255, 255, 255, 255),
+            padding: const EdgeInsets.all(5),
+            child: const Text("OK"),
+          ),
+        ),
+      ],
+    );
