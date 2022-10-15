2ed914120928e617afb87b1c6bad9294f2d08d72
Chathura <chathura.madura11@gmail.com>
2022-10-15 20:09:40 +0530

    view payment detail view

---
+++ lib/screens/classfee/payment_details.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class PaymentDetails extends StatefulWidget {
+  const PaymentDetails({super.key});
+
+  @override
+  State<PaymentDetails> createState() => _PaymentDetailsState();
+}
+
+class _PaymentDetailsState extends State<PaymentDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("see payment details in here!"),
+    );
+  }
+}
