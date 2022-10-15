0fafe2e849cf867ad98455bae1a774021e05be92
Chathura <chathura.madura11@gmail.com>
2022-10-16 00:17:14 +0530

    add new field to user

--- lib/services/usermanagement.dart
+++ lib/services/usermanagement.dart
@@ -4,13 +4,13 @@ import 'package:flutter/cupertino.dart';
 
 class Usermanagement {
   CollectionReference ref = FirebaseFirestore.instance.collection('/users');
-  storeNewUser(UserCredential value, String role, context) {
+  storeNewUser(UserCredential value, String role,String username, context) {
     var email = value.user!.email;
     var pw = value.user!.uid;
     var userrole = "";
     (role == "Userrole.teacher") ? userrole = "teacher" : userrole = "student";
       
-    ref.add({'email': email, 'password': pw, 'role': userrole}).then((value) {
+    ref.add({'email': email, 'password': pw, 'role': userrole,'name':username}).then((value) {
       Navigator.of(context).pop();
       Navigator.pushNamed(context, '/landingpage');
     }).catchError((e) {
