88a713ed2324792298272bde14cac255fcb26d2f
Chathura <chathura.madura11@gmail.com>
2022-11-06 23:02:15 +0530

    change login page styles

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -23,116 +23,126 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(66, 238, 252, 250),
-        body: Padding(
-            padding: const EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(10),
-                    child: const Text(
-                      'Welcome to Smart Academy!',
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 0, 60, 108),
-                          fontWeight: FontWeight.w500,
-                          fontSize: 30),
-                    )),
-                Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(10),
-                    child: const Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 20),
-                    )),
-                Container(
+      // backgroundColor: Color.fromARGB(66, 238, 252, 250),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    "https://images.unsplash.com/photo-1509228627152-72ae9ae6848d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hdGhlbWF0aWNzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"),
+                fit: BoxFit.cover)),
+        padding: EdgeInsets.only(top: 50.0),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
                   padding: const EdgeInsets.all(10),
-                  child: TextField(
-                      // controller: nameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          _email = value;
-                        });
-                      }),
-                ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                      obscureText: true,
-                      // controller: passwordController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          _password = value;
-                        });
-                      }),
-                ),
-                Container(
-                    height: 50,
-                    margin: const EdgeInsets.only(
-                        top: 10.0),
-                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: ElevatedButton(
-                      child: const Text('Login'),
-                      onPressed: () {
-                        // print(nameController.text);
-                        // print(passwordController.text);
-                        // print("pw:"+_password+ "| email :" +_email);
-                        FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
-                                email: _email, password: _password)
-                            .then((user) {
-                          // Query for get user role
-                          CollectionReference currentUser =
-                              FirebaseFirestore.instance.collection('users');
-                          FirebaseFirestore.instance
-                              .collection('users')
-                              .where('email', isEqualTo: _email)
-                              .get()
-                              .then((QuerySnapshot querySnapshot) {
-                            querySnapshot.docs.forEach((doc) {
-                              print(doc["role"]);
-                              setState(() {
-                                _role = doc["role"];
-                              });
-                              if (_role == "student") {
-                                Navigator.of(context)
-                                    .pushReplacementNamed('/home');
-                              } else if (_role == "teacher") {
-                                Navigator.of(context)
-                                    .pushReplacementNamed('/teacherhome');
-                              }
+                  child: const Text(
+                    'Welcome to Smart Academy!',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 60, 108),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 20),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                    // controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _email = value;
+                      });
+                    }),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextField(
+                    obscureText: true,
+                    // controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _password = value;
+                      });
+                    }),
+              ),
+              Container(
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 10.0),
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('Login'),
+                    onPressed: () {
+                      // print(nameController.text);
+                      // print(passwordController.text);
+                      // print("pw:"+_password+ "| email :" +_email);
+                      FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                              email: _email, password: _password)
+                          .then((user) {
+                        // Query for get user role
+                        CollectionReference currentUser =
+                            FirebaseFirestore.instance.collection('users');
+                        FirebaseFirestore.instance
+                            .collection('users')
+                            .where('email', isEqualTo: _email)
+                            .get()
+                            .then((QuerySnapshot querySnapshot) {
+                          querySnapshot.docs.forEach((doc) {
+                            print(doc["role"]);
+                            setState(() {
+                              _role = doc["role"];
                             });
+                            if (_role == "student") {
+                              Navigator.of(context)
+                                  .pushReplacementNamed('/home');
+                            } else if (_role == "teacher") {
+                              Navigator.of(context)
+                                  .pushReplacementNamed('/teacherhome');
+                            }
                           });
-                          // save user email in sharedpref
-
-                          saveUserEmail() async {
-                            SharedPreferences prefs =
-                                await SharedPreferences.getInstance();
-                            prefs.setString("useremail", _email);
-                          }
-
-                          saveUserEmail();
-                          // redirect home page based on user
-                        }).catchError((e) {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return alert(context);
-                            },
-                          );
-                          // print(e);
                         });
-                      },
-                    )),
-                Row(
+                        // save user email in sharedpref
+
+                        saveUserEmail() async {
+                          SharedPreferences prefs =
+                              await SharedPreferences.getInstance();
+                          prefs.setString("useremail", _email);
+                        }
+
+                        saveUserEmail();
+                        // redirect home page based on user
+                      }).catchError((e) {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return alert(context);
+                          },
+                        );
+                        // print(e);
+                      });
+                    },
+                  )),
+              Padding(
+                padding: EdgeInsets.only(top: 150.0),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     const Text('Does not have account?'),
@@ -147,8 +157,12 @@ class _LoginPageState extends State<LoginPage> {
                     )
                   ],
                 ),
-              ],
-            )));
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
